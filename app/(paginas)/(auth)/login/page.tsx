fb2e101084e0f4e64f842aa8008743530ac238d8
pc home <chaman183000@gmail.com>
2026-03-04 12:28:41 -0400

    Resuelto register

--- app/(paginas)/(auth)/login/page.tsx
+++ app/(paginas)/(auth)/login/page.tsx
@@ -1,6 +1,4 @@
-
-
-export default function Home() {
+export default function Login() {
   return (
     <div className="flex min-h-screen items-center justify-center">
       <main className="flex min-h-screen w-full max-w-3xl flex-col items-center justify-between py-32 px-16 sm:items-start">
---
+++ app/(paginas)/(auth)/register/page.tsx
@@ -0,0 +1,9 @@
+export default function Register() {
+  return (
+    <div className="flex min-h-screen items-center justify-center">
+      <main className="flex min-h-screen w-full max-w-3xl flex-col items-center justify-between py-32 px-16 sm:items-start">
+    login
+      </main>
+    </div>
+  );
+}
